04ddc343dc5ab74be208381f281f5efb908fdf24
Enzo Batista <batista.enzo.be@gmail.com>
2024-03-18 20:37:06 +0100

    first commit

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php 
+
+add_action( 'wp_enqueue_scripts', 'chicdressing_enqueue_styles' );
+function chicdressing_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' ); 
+}
+
+add_filter( 'big_image_size_threshold', '__return_false' );
+
+function ashe_gfonts_scripts_custom() {
+    wp_dequeue_style( 'ashe-playfair-font', ashe_playfair_font_url(), array(), '1.0.0' );
+    wp_dequeue_style( 'ashe-opensans-font', ashe_opensans_font_url(), array(), '1.0.0' );
+}
+add_action( 'wp_enqueue_scripts', 'ashe_gfonts_scripts_custom' );
+
+
+ 
